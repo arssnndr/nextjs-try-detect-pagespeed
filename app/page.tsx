1e1534a464a7ccda2e48cf208276222ed9f7802b
ARIS <aris.sunandar@salt.co.id>
2024-12-19 17:36:32 +0700

    change from CSR to SSR

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { headers } from "next/headers";
 export default async function Home() {
   const headersList = headers();
   const userAgent = (await headersList).get("user-agent") || "";
-  // const isLighthouse = userAgent.includes("Lighthouse");
+  const isLighthouse = userAgent.includes("Lighthouse");
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -14,33 +14,19 @@ export default async function Home() {
           <span className="text-[#f7ab0a]"> {userAgent}</span>
         </h1>
 
+        <br />
+        <br />
+        <br />
+
+        {isLighthouse && (
+          <>
+          <h1 className="text-4xl sm:text-6xl font-bold">
+            Lighthouse detected!
+          </h1>
+          </>
+        )}
+
       </main>
     </div>
   );
 }
-
-
-// "use client"
-
-// import { useEffect, useState } from "react";
-
-// export default function Home() {
-//   const [userAgent, setUserAgent] = useState("");
-
-//   useEffect(() => {
-//     setUserAgent(navigator.userAgent);
-//   }, []);
-
-//   return (
-//     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-//       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-//         <h1 className="text-4xl sm:text-6xl font-bold">
-//           User Agent:
-//           <span className="text-[#f7ab0a]"> {userAgent}</span>
-//         </h1>
-
-//       </main>
-//     </div>
-//   );
-// }
